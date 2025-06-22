be006ab1c4d201812825a100394b3b0f98ab46c5
Jiven Timsina <timsinajiven00@gmail.com>
2025-06-22 17:24:55 +0545

    update setup file

--- setup.sh
+++ setup.sh
@@ -13,3 +13,27 @@ ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
 
 #vim
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+
+#kitty
+ln -sf ~/.dotfiles/kitty ~/.config/kitty
+
+#hyprland
+ln -sf ~/.dotfiles/hypr ~/.config/hypr
+
+#waybar
+ln -sf ~/.dotfiles/waybar ~/.config/waybar
+
+# fastfetch
+ln -sf ~/.dotfiles/fastfetch ~/.config/fastfetch
+
+#dunst
+ln -sf ~/.dotfiles/dunst ~/.config/dunst
+
+#rofi
+ln -sf ~/.dotfiles/rofi ~/.config/rofi
+
+#jome
+ln -sf ~/.dotfiles/jome ~/.config/jome
+
+#tofi
+ln -sf ~/.dotfiles/tofi ~/.config/tofi
