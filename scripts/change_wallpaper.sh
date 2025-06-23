e27dbdc34438ece3d9169630c454ef77131780fd
Jiven Timsina <timsinajiven00@gmail.com>
2025-06-23 13:46:28 +0545

    change wallpaper time

--- scripts/change_wallpaper.sh
+++ scripts/change_wallpaper.sh
@@ -6,5 +6,5 @@ WALLPAPERS_DIR="$HOME/.dotfiles/nord-wallpaper"
 while true; do
     IMG=$(find "$WALLPAPERS_DIR" -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)
     swww img "$IMG" --transition-type grow --transition-fps 60 --transition-duration 2 --transition-pos 0.5,0.5
-    sleep 300 # 5 minutes
+    sleep 900 # 15 minutes
 done
