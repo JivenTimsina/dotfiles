27016eefb52bd7c130b797b74356b83dd5aa0fff
Jiven Timsina <timsinajiven00@gmail.com>
2025-06-27 12:18:46 +0545

    change wallpaper using bind

--- scripts/change_wallpaper.sh
+++ scripts/change_wallpaper.sh
@@ -3,8 +3,5 @@
 WALLPAPERS_DIR="$HOME/.dotfiles/nord-wallpaper"
 
 # choose random image from the WALLPAPERS_DIR and change wallpaper
-while true; do
-    IMG=$(find "$WALLPAPERS_DIR" -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)
-    swww img "$IMG" --transition-type grow --transition-fps 60 --transition-duration 2 --transition-pos 0.5,0.5
-    sleep 900 # 15 minutes
-done
+IMG=$(find "$WALLPAPERS_DIR" -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)
+swww img "$IMG" --transition-type grow --transition-fps 60 --transition-duration 2 --transition-pos 0.5,0.5
