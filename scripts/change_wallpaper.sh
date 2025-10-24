e336e3b3a4acbec5cbf9cd3f4d0572c7a77412b1
Jiven Timsina <timsinajiven00@gmail.com>
2025-10-24 13:00:32 +0545

    update wallpaper transition to any

--- scripts/change_wallpaper.sh
+++ scripts/change_wallpaper.sh
@@ -4,4 +4,4 @@ WALLPAPERS_DIR="$HOME/.dotfiles/wallpapers/wall"
 
 # choose random image from the WALLPAPERS_DIR and change wallpaper
 IMG=$(find "$WALLPAPERS_DIR" -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)
-swww img "$IMG" --transition-type grow --transition-fps 60 --transition-duration 2 --transition-pos 0.5,0.5
+swww img "$IMG" --transition-type any --transition-fps 60 --transition-duration 2 
