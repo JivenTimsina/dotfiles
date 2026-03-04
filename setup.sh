70a75d72e45675f80e91160d8124b1890ef56479
Jiven Timsina <timsinajiven00@gmail.com>
2026-03-04 08:31:24 +0545

    update

--- setup.sh
+++ setup.sh
@@ -15,13 +15,13 @@ ln -sf "$DOTFILES/wezterm/wezterm.lua" ~/.wezterm.lua
 ln -sf "$DOTFILES/nvim" ~/.config/nvim
 ln -sf "$DOTFILES/zsh/.zshrc" ~/.zshrc
 ln -sf "$DOTFILES/vim/vimrc" ~/.vimrc
-ln -sf "$DOTFILES/hypr" ~/.config/hypr
-ln -sf "$DOTFILES/waybar" ~/.config/waybar
 ln -sf "$DOTFILES/fastfetch" ~/.config/fastfetch
-ln -sf "$DOTFILES/dunst" ~/.config/dunst
-ln -sf "$DOTFILES/rofi" ~/.config/rofi
-ln -sf "$DOTFILES/jome" ~/.config/jome
-ln -sf "$DOTFILES/tofi" ~/.config/tofi
 ln -sf "$DOTFILES/zathura" ~/.config/zathura
 ln -sf "$DOTFILES/kitty" ~/.config/kitty
 sudo ln -sf "$DOTFILES/greetd" /etc/greetd
+sudo ln -sf "$DOTFILES/i3status-rust" ~/.config/i3status-rust
+sudo ln -sf "$DOTFILES/mako" ~/.config/mako
+sudo ln -sf "$DOTFILES/sway" ~/.config/sway
+sudo ln -sf "$DOTFILES/swaylock" ~/.config/swaylock
+sudo ln -sf "$DOTFILES/yazi" ~/.config/yazi
+
