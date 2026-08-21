7a80c0bcf65b4072401e04de82235b3b25acf7cc
JivenTimsina <timsinajiven00@gmail.com>
2026-08-21 17:20:18 +0545

    update setup script

--- setup.sh
+++ setup.sh
@@ -15,13 +15,10 @@ ln -sf "$DOTFILES/wezterm/wezterm.lua" ~/.wezterm.lua
 ln -sf "$DOTFILES/nvim" ~/.config/nvim
 ln -sf "$DOTFILES/zsh/.zshrc" ~/.zshrc
 ln -sf "$DOTFILES/vim/vimrc" ~/.vimrc
-ln -sf "$DOTFILES/fastfetch" ~/.config/fastfetch
 ln -sf "$DOTFILES/zathura" ~/.config/zathura
 ln -sf "$DOTFILES/kitty" ~/.config/kitty
-sudo ln -sf "$DOTFILES/greetd" /etc/greetd
 sudo ln -sf "$DOTFILES/i3status-rust" ~/.config/i3status-rust
 sudo ln -sf "$DOTFILES/mako" ~/.config/mako
 sudo ln -sf "$DOTFILES/sway" ~/.config/sway
 sudo ln -sf "$DOTFILES/swaylock" ~/.config/swaylock
 sudo ln -sf "$DOTFILES/yazi" ~/.config/yazi
-sudo ln -sf "$DOTFILES/grub/grub" /etc/default/grub
