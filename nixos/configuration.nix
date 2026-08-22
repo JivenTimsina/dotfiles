7b858b8a744830449e452517c166ec04356e2701
JivenTimsina <timsinajiven00@gmail.com>
2026-08-22 09:41:25 +0545

    nixos

---
+++ nixos/configuration.nix
@@ -0,0 +1,231 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+   networking.hostName = "nixos"; # Define your hostname.
+
+  # Configure network connections interactively with nmcli or nmtui.
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+   time.timeZone = "Asia/Kathmandu";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+
+  
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # services.pulseaudio.enable = true;
+  # OR
+   services.pipewire = {
+     enable = true;
+     pulse.enable = true;
+   };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+   services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+ users.users.jiven = {
+   shell = pkgs.zsh;
+   isNormalUser = true;
+   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+   packages = with pkgs; [
+     tree
+   ];
+ };
+
+   	programs.firefox.enable = true;
+	programs.sway.enable = true;
+  	programs.zsh.enable = true;
+
+xdg.portal = {
+    enable = true;
+    wlr = {
+      enable = true;
+      settings = {
+        screencast = {
+          # Force simple selection mode
+          chooser_type = "simple";
+          # Use the exact absolute store path for slurp
+          chooser_cmd = "${pkgs.slurp}/bin/slurp -f %o -or";
+        };
+      };
+    };
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    config.common.default = [ "wlr" "gtk" ];
+  };
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+   environment.systemPackages = with pkgs; [
+     	vim
+	wget
+	greetd
+	curl
+	sway
+	firefox
+	git
+	kitty
+	neovim
+	zsh
+	libnotify
+	adwaita-icon-theme
+	jq
+	i3status-rust
+	mako
+	zathura
+	awww
+	mpv	
+	imv
+	bat
+	yazi
+	imagemagick
+	wl-clipboard
+	obs-studio
+	fzf
+	clipman
+	grim
+	slurp
+	gammastep
+	calc
+
+	# development
+
+	# C / C++
+	gcc
+	gnumake
+	gdb
+	cmake
+	clang
+	clang-tools
+
+	# lua
+	lua
+	luarocks
+	lua-language-server
+
+	# Rust
+	cargo
+	rustc
+	rust-analyzer
+
+	# Node.js & Version Manager
+	vscode-langservers-extracted
+	nodejs
+	typescript-go
+
+	# Bash
+	bash-language-server
+
+	# Go
+	go
+	gopls
+	go-tools
+
+	# Typesetting
+	typst
+	typstyle
+	tinymist
+
+	# Python
+	python3
+	pyright
+   ];
+
+   programs.nix-ld.enable = true;
+
+   fonts = {
+	   packages = with pkgs; [
+		   nerd-fonts.jetbrains-mono
+	   ];
+   };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+   services.flatpak.enable = true;
+   services.greetd = {
+	   enable = true;
+	   settings = {
+		   default_session = {
+			   command = "${pkgs.tuigreet}/bin/tuigreet -w 60 --remember --remember-session --asterisks  --greet-align left --time --user-menu --theme 'border=purple;text=purple;prompt=green;time=red;action=white;button=white;container=transparent;input=white' --cmd sway";
+			   user = "greeter";
+		   };
+	   };
+   };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+   system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "26.05"; # Did you read the comment?
+
+}
+
--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@
 
 set -e # Exit on any error
 
+# exec as sudo
+if [[ $EUID -ne 0 ]]; then
+    exec sudo "$0" "$@"
+fi
+
 # create symlinks
 DOTFILES="$HOME/.dotfiles"
 if [[ ! -d "$DOTFILES" ]]; then
@@ -11,14 +16,15 @@ fi
 
 echo "Creating symlinks..."
 
+ln -sfn "$DOTFILES/nixos" /etc/nixos
 ln -sf "$DOTFILES/wezterm/wezterm.lua" ~/.wezterm.lua
 ln -sf "$DOTFILES/nvim" ~/.config/nvim
-ln -sf "$DOTFILES/zsh/.zshrc" ~/.zshrc
+ln -sf "$DOTFILES/zsh/zshrc" ~/.zshrc
 ln -sf "$DOTFILES/vim/vimrc" ~/.vimrc
 ln -sf "$DOTFILES/zathura" ~/.config/zathura
 ln -sf "$DOTFILES/kitty" ~/.config/kitty
-sudo ln -sf "$DOTFILES/i3status-rust" ~/.config/i3status-rust
-sudo ln -sf "$DOTFILES/mako" ~/.config/mako
-sudo ln -sf "$DOTFILES/sway" ~/.config/sway
-sudo ln -sf "$DOTFILES/swaylock" ~/.config/swaylock
-sudo ln -sf "$DOTFILES/yazi" ~/.config/yazi
+ln -sf "$DOTFILES/i3status-rust" ~/.config/i3status-rust
+ln -sf "$DOTFILES/mako" ~/.config/mako
+ln -sf "$DOTFILES/sway" ~/.config/sway
+ln -sf "$DOTFILES/swaylock" ~/.config/swaylock
+ln -sf "$DOTFILES/yazi" ~/.config/yazi
